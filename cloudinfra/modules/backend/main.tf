c2469c50e3a26fa73cfbf940fe6a0b2bda8d09cd
Oluwadamilola Oyaluade <167680028+Olu199@users.noreply.github.com>
2025-04-25 10:09:31 -0500

    Update main.tf

--- cloudinfra/modules/backend/main.tf
+++ cloudinfra/modules/backend/main.tf
@@ -3,23 +3,30 @@
 resource "aws_s3_bucket" "tf_state" {
   bucket = var.bucket_name
 
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
   }
 }
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+
 
 resource "aws_dynamodb_table" "tf_locks" {
   name           = var.lock_table_name
