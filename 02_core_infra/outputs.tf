b040130d4cc4b930725ff6104388313a67ce6009
root <oyaluadedamilola@damilstudio.com>
2025-05-12 18:14:39 -0500

    refactor: add output for organiztion module to root outputs.tf

--- 02_core_infra/outputs.tf
+++ 02_core_infra/outputs.tf
@@ -23,3 +23,24 @@ output "private_subnet_ids" {
   description = "List of private subnet IDs"
   value       = module.vpc.private_subnet_ids
 }
+
+
+
+output "organization_account_details" {
+  description = "All organization account details"
+  value = {
+    developer_email = {
+        name = module.developer_account_name
+        email = module.developer_account_email
+      }
+          production_email = {
+        name = module.production_account_name
+        email = module.production_account_email
+      }
+          machine_learning_email = {
+        name = module.machine_learning_account_name
+        email = module.machine_learning_account_email
+      }
+    }
+  }
+
--- 03_modules/org_structure/outputs.tf
+++ 03_modules/org_structure/outputs.tf
@@ -3,7 +3,15 @@ output "local_values" {
   value = {
     developer_email = {
         name = local.developer_account_name
-        name = local.developer_account_email
+        email = local.developer_account_email
+      }
+          production_email = {
+        name = local.production_account_name
+        email = local.production_account_email
+      }
+          machine_learning_email = {
+        name = local.machine_learning_account_name
+        email = local.machine_learning_account_email
       }
     }
   }
